cdd74f5aed1cbcaf88d465652a62ca1e8c76e8cc
Luis-Betancourt93 <betancourtluis619@gmail.com>
2022-12-08 22:41:13 -0800

    Got Mobile first working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif"
+      }
+    },
   },
   plugins: [],
 }
